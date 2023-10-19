b377e1759ca59de9206bd3ed37d61c65486890b0
CavRiley <cavan-riley@uiowa.edu>
2023-10-19 14:51:29 -0500

    Tests working, relatively, for finding valid dcms and modality

--- testing/integration_testing/test_valid_dcm.py
+++ testing/integration_testing/test_valid_dcm.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pydicom
 import pytest
+from dcm_classifier.dicom_volume import DicomSingleVolumeInfoBase
 from dcm_classifier.image_type_inference import ImageTypeClassifierBase
 from dcm_classifier.study_processing import ProcessOneDicomStudyToVolumesMappingBase
 from pydicom.errors import InvalidDicomError
@@ -13,15 +14,29 @@ inference_model_path = list(Path(__file__).parent.parent.parent.rglob("models/rf
 def test_dcm_validity():
     inferer = ImageTypeClassifierBase(classification_model_filename=inference_model_path)
     study = ProcessOneDicomStudyToVolumesMappingBase(
-        study_directory="testing/dcm_files", inferer=inferer
+        study_directory=current_file_path.parent / "dcm_files", inferer=inferer
         )
-    study.run_inference()
+    # study.run_inference()
 
     volumes = study.get_list_of_primary_volume_info()
-
+    dict = study.series_dictionary
+    print(dict.get(1).get_modality())
+    print(volumes)
     assert len(volumes) == 1
 
 
-    # with pytest.raises(InvalidDicomError) as ex:
-    #
-    # assert "File is missing DICOM File Meta Information" in str(ex.value)
+def test_mr_dcm():
+    inferer = ImageTypeClassifierBase(classification_model_filename=inference_model_path)
+    study = ProcessOneDicomStudyToVolumesMappingBase(
+        study_directory=current_file_path.parent.parent.parent / "dcm_files", inferer=inferer
+        )
+    # study.run_inference()
+
+    print(study.get_list_of_primary_volume_info())
+    print(study.series_dictionary.get(3).volume_info_list)
+    assert not DicomSingleVolumeInfoBase([current_file_path.parent / "dcm_files/new_MRBRAIN.DCM"]).get_modality() == "MR"
+
+    # print(study.get_list_of_primary_volume_info())
+    # assert study.get_list_of_primary_volume_info()[0]["Modality"] == "MR"
+
+
