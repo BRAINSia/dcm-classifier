33deb576de343b383c364ebd50903360bf6ee358
CavRiley <cavan-riley@uiowa.edu>
2023-10-19 16:53:52 -0500

    Small edits for DICOM files, still not valid files

--- testing/dcm_files/edit_dcm_files.py
+++ testing/dcm_files/edit_dcm_files.py
@@ -17,12 +17,12 @@ file_meta.ImplementationVersionName = "PYDICOM"
 file_meta.ImplementationClassUID = "1.3.46.670589.17"
 
 
-ds = FileDataset("valid_CT_file.dcm", {}, file_meta=file_meta, preamble=b"\0" * 128)
+ds = FileDataset("a_valid_file.dcm", {}, file_meta=file_meta, preamble=b"\0" * 128)
 # ds = pydicom.read_file("valid_CT_file.dcm")
 
 ds.PatientName = "Test^Firstname"
 ds.PatientID = "123456"
-ds.Modality = "CT"
+ds.Modality = "MR"
 ds.StudyDate = "20190101"
 ds.StudyTime = "123456"
 ds.StudyID = "123456789"
@@ -38,9 +38,9 @@ ds.is_little_endian = True
 ds.is_implicit_VR = True
 ds.ClinicalTrialSponsorName = "Test Sponsor"
 ds.ClinicalTrialProtocolID = "Test Protocol ID"
-ds.StudyInstanceUID = "1.3.46.670589.17"
-ds.SeriesInstanceUID = "1.3.46.670589.18"
-ds.FrameOfReferenceUID = "1.3.46.670589.19"
+ds.StudyInstanceUID = UID("1.2.826.0.1.3680043.2.1125.1.38381854871216336385978062044218957")
+ds.SeriesInstanceUID = UID("1.2.826.0.1.3680043.2.1125.1.68878959984837726447916707551399667")
+ds.FrameOfReferenceUID = UID("1.3.46.670589.19")
 ds.ImagePositionPatient = [0, 0, 0]
 ds.ImageOrientationPatient = [1, 0, 0, 0, 1, 0]
 ds.PixelSpacing = [1, 1]
@@ -55,12 +55,14 @@ ds.PixelRepresentation = 0
 ds.ImageType = ["ORIGINAL", "PRIMARY", "OTHER"]
 ds.ScanningSequence = "GR"
 ds.SequenceVariant = "NONE"
-ds.SOPClassUID = "1.2.840.10008.5.1.4.1.1.4"
-ds.SOPInstanceUID = "1.3.6.1.4.1.9328.50.51.2674852332200054861"
+ds.SOPClassUID = UID("1.2.840.10008.5.1.4.1.1.4")
+ds.SOPInstanceUID = UID("1.3.6.1.4.1.9328.50.51.2674852332200054861")
 ds.Manufacturer = "Test Manufacturer"
 ds.EchoTime = 1
 ds.RepetitionTime = 1
 ds.FlipAngle = 1
+ds.MRAcquisitionType = "2D"
+ds.SliceThickness = 1
 
 ds.PixelBandwidth = 100
 ds.SAR = 100
@@ -74,7 +76,7 @@ ds.RescaleSlope = 1
 # ds.SAR = 100
 
 print(ds)
-dcmwrite("valid_CT_file.dcm", ds)  # use `dcmwrite()` if creating file from scratch, `save_as()` doesn't include header info
+dcmwrite("a_valid_file.dcm", ds)  # use `dcmwrite()` if creating file from scratch, `save_as()` doesn't include header info
 # # ds.save_as("MRBRAIN.DCM")
 # dcmwrite("new_MRBRAIN.DCM", ds)
 print("*" * 80)
