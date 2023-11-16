72a587f6983c7aea4f588151496700212c63887d
mbrzus <michal-brzus@uiowa.edu>
2023-11-15 19:29:13 -0600

    STYLE: format test suite with black

--- tests/unit_testing/test_dicom_validator.py
+++ tests/unit_testing/test_dicom_validator.py
@@ -15,18 +15,20 @@ def test_volume_validation(mock_volume_study, mock_volumes):
     assert "testing" in report and "This is a TEST" in report
 
 
-
 # Always going to pass, `validate()` returns True
 def test_validate(mock_volumes):
     validator = DicomValidatorBase(DicomSingleVolumeInfoBase(mock_volumes[0]))
     assert validator.validate() is not False
+
 
 def test_write_validation_report(mock_volumes):
     validator = DicomValidatorBase(DicomSingleVolumeInfoBase(mock_volumes[0]))
     validator.append_to_validation_failure_reports("testing")
     validator.append_to_validation_failure_reports("This is a TEST")
 
-    test_report_path: Path = Path(__file__).parent.parent / "testing_data" / "test_report.txt"
+    test_report_path: Path = (
+        Path(__file__).parent.parent / "testing_data" / "test_report.txt"
+    )
     validator.write_validation_report(test_report_path)
     assert test_report_path.exists()
     assert test_report_path.is_file()
@@ -37,6 +39,7 @@ def test_write_validation_report(mock_volumes):
 
     test_report_path.unlink(missing_ok=True)
 
+
 def test_write_validation_report_append(mock_volumes, capsys):
     validator = DicomValidatorBase(DicomSingleVolumeInfoBase(mock_volumes[0]))
     validator.append_to_validation_failure_reports("testing")
@@ -44,4 +47,3 @@ def test_write_validation_report_append(mock_volumes, capsys):
     captured = capsys.readouterr()
     print(captured.out)
     assert "testing" in captured.out and "Identified bvalue: -12345" in captured.out
-
