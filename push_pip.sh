924c18d67517a2f9afbf0eaf185e9572e88acdd8
Hans Johnson <hans-johnson@uiowa.edu>
2024-02-21 12:28:30 -0600

    STYLE: Enforce shell check to imporove robustness of shell scripts.

--- push_pip.sh
+++ push_pip.sh
@@ -3,19 +3,18 @@
 echo Deleting existing dist folder
 rm -rf dist
 echo Enter desired version number
-read version
+read -r version
 
-cat pyproject.txt \
- | sed  "s#version = '0.0.1'#version='${version}'#g" \
- > pyproject.toml
+sed  "s#version = '0.0.1'#version='${version}'#g" \
+ < pyproject.txt > pyproject.toml
 
 python3 -m build
 
-echo Upload new version $version to PyPi '(y,n)'?
-read decision
+echo "Upload new version ${version} to PyPi '(y,n)'?"
+read -r decision
 
-if [ $decision = "Y" ] || [ $decision = 'y' ]; then
+if [ "${decision}" = "Y" ] || [ "${decision}" = 'y' ]; then
   twine upload dist/*
 else
-  echo Exiting program
+  echo "Exiting program"
 fi
