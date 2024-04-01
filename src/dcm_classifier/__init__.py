83072f360cded880ed246b4430c02028c1309ef2
Michal Brzus <michal-brzus@uiowa.edu>
2024-04-01 15:32:28 -0500

    ENH: resolve bug in development for accessing package version

--- src/dcm_classifier/__init__.py
+++ src/dcm_classifier/__init__.py
@@ -1,3 +1,8 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version("dcm_classifier")
+try:
+    # Attempt to retrieve the version of the installed package
+    __version__ = importlib.metadata.version("dcm_classifier")
+except importlib.metadata.PackageNotFoundError:
+    # Fallback version if the package is not installed (e.g., in development)
+    __version__ = "0.0.0-dev"
