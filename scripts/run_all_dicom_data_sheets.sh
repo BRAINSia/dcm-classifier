49ec93b6b6f261c16999f36a6d5a7ffb91abc7c2
CavRiley <cavan-riley@uiowa.edu>
2024-04-02 11:39:36 -0500

    STYLE: commented script for pre-commits to run w/o fail
    WIP: check precommit doesnt fail on run all datasheets script
    
    STYLE: commented script to make precommit run

--- scripts/run_all_dicom_data_sheets.sh
+++ scripts/run_all_dicom_data_sheets.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-export PYTHONPATH=<PATH_TO_dcm-classifier_SRC_DIR>
-source <PATH_TO_PYTHON_VENV>/bin/activate
+echo To use this script, remove the multiline comment and fill in paths as needed.
+
+# export PYTHONPATH=<PATH_TO_dcm-classifier_SRC_DIR>
+# source <PATH_TO_PYTHON_VENV>/bin/activate
+#
+#
+# cat > todo_list << EOF
+# list
+# of
+# projects
+# to
+# process
+# EOF
 
 
-cat > todo_list << EOF
-list
-of
-projects
-to
-process
-EOF
-
-
-while IFS= read -r proj; do
-  data_table_file=${HOME}/<PATH_TO_DATA_OUTPUTS>/${proj}.xlsx
-#   shellcheck disable=SC2086
-  mkdir -p "$(dirname ${data_table_file})"
-  if [ -f "${data_table_file}" ]; then
-    echo "Skipping ${proj}"
-  else
-    echo "${proj} starting"
-    if python3 create_dicom_fields_sheet.py --dicom_path "PATH_TO_PROJECTS/${proj}" --out "${data_table_file}" ; then
-	    echo "SUCCESS PROCESSING"
-	    echo "FAILED PROCESSING"
-            echo "python3 create_dicom_fields_sheet.py --dicom_path PATH_TO_PROJECTS/${proj} --out ${data_table_file}"
-	    break
-    fi
-    echo "${proj} stopping"
-  fi
-done < todo_list
+# while IFS= read -r proj; do
+#   data_table_file=${HOME}/<PATH_TO_DATA_OUTPUTS>/${proj}.xlsx
+# #   shellcheck disable=SC2086
+#   mkdir -p "$(dirname ${data_table_file})"
+#   if [ -f "${data_table_file}" ]; then
+#     echo "Skipping ${proj}"
+#   else
+#     echo "${proj} starting"
+#     if python3 create_dicom_fields_sheet.py --dicom_path "PATH_TO_PROJECTS/${proj}" --out "${data_table_file}" ; then
+# 	    echo "SUCCESS PROCESSING"
+# 	    echo "FAILED PROCESSING"
+#             echo "python3 create_dicom_fields_sheet.py --dicom_path PATH_TO_PROJECTS/${proj} --out ${data_table_file}"
+# 	    break
+#     fi
+#     echo "${proj} stopping"
+#   fi
+# done < todo_list
