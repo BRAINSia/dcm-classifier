feb0e5af83cfcc329254b6d489e973a8f006eec4
CavRiley <cavan-riley@uiowa.edu>
2024-04-16 12:19:08 -0500

    ADD: added documentation files from docs dir

---
+++ docs/source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
+
+
+project = "dcm-classifier"
+copyright = "2024, Michal Brzus and Hans Johnson and Cavan Riley"
+author = "Michal Brzus and Hans Johnson and Cavan Riley"
+release = "0.8.2"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",  # Numpy style docstrings
+    # "sphinx.ext.linkcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+master_doc = "index"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+html_theme = "sphinx_rtd_theme"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+# html_theme_options = {}
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ docs/source/dcm_classifier.rst
@@ -0,0 +1,61 @@
+dcm\_classifier package
+=======================
+
+Submodules
+----------
+
+
+.. automodule:: dcm_classifier.dicom_config
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.dicom_series
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.dicom_validator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.dicom_volume
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.example_image_processing
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.image_type_inference
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.study_processing
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+.. automodule:: dcm_classifier.utility_functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: dcm_classifier
+   :members:
+   :undoc-members:
+   :show-inheritance:
